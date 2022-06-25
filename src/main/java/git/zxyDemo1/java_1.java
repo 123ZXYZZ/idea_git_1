b29ed188a6af580b294a291ba9af7329075f2eef
123ZXYZZ <87707219+123ZXYZZ@users.noreply.github.com>
2022-06-25 14:25:57 +0800

    hithub_2

--- src/main/java/git/zxyDemo1/java_1.java
+++ src/main/java/git/zxyDemo1/java_1.java
@@ -4,9 +4,7 @@ public class java_1 {
     public static void main(String[] args) {
         System.out.println("git_1");
         System.out.println("git_2");
-        System.out.println("git_2");
-        System.out.println("git_2");
-        System.out.println("megr");
+        
         System.out.println("mastertext");
 
         System.out.println("mastertext");
